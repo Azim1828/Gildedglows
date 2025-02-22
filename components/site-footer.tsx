84217895613346106d7ad4f59f54653f0db49756
azim128 <azimruet28@gmail.com>
2025-02-22 10:14:50 +0600

    update

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -24,6 +24,7 @@ export function SiteFooter() {
                 JiangSuShengSuQianShi, 223800, China
               </p>
               <p>baisdejacqui233784@outlook.com</p>
+              <p>Contact Person: Wang Liu</p>
             </div>
           </div>
           {/* Information */}
